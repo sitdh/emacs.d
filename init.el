89df53091de2bc40e45a4d0c6a4cb6fe9454383e
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-01-28 00:06:53 +0700

    Update emacs script

--- init.el
+++ init.el
@@ -5,3 +5,9 @@
 (add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp" t)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(global-set-key "\C-cd" "\C-a\C- \C-n\M-w\C-y")
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)

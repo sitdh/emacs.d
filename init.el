d67ddc7f42ef7d24938b582ccde383e850b31a0e
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-01-28 11:26:12 +0700

    Remove unnecessary lines

--- init.el
+++ init.el
@@ -1,17 +1,18 @@
 (require 'package)
 (package-initialize)
 
-; (setq debug-on-error t
-;       debug-on-signal nil
-;       debug-on-quit nil)
-  
 (add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
 (add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp" t)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-(global-set-key "\C-cd" "\C-a\C- \C-n\M-w\C-y")
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+;;osx keys
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'alt)

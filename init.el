ca2ece17005e6e55a3d81d71c1e6aab346d72583
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-02-03 22:28:45 +0700

    Remove manual install and add org-mode via ELPA

--- init.el
+++ init.el
@@ -2,9 +2,7 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(add-to-list 'load-path "~/.emacs.d/plugins/org-mode/lisp")
-(add-to-list 'load-path "~/.emacs.d/plugins/org-mode/contrib/lisp" t)
-(add-to-list 'load-path "~/.emacs.d/plugins/evil")
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
@@ -27,3 +25,15 @@
         (fill-region (region-beginning) (region-end)))
       (dotimes (_ (abs n))
         (delete-indentation (natnump n)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
